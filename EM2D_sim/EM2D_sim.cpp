56d69c27f7046c0c447154311912699b548daec0
Aias Tatsis <aias.v.tatsis@gmail.com>
2025-10-06 20:54:50 -0400

    Add project files.

---
+++ EM2D_sim/EM2D_sim.cpp
@@ -0,0 +1,12 @@
+﻿// EM2D_sim.cpp : Defines the entry point for the application.
+//
+
+#include "EM2D_sim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
